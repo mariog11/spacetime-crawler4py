2dd8a300e6ac6deb5b9ae6140da3ca263965f07a
Mario Gomez <mariogomez1108@yahoo.com>
2019-10-31 10:15:29 -0700

    Handle redirects, for real. Handle relative links, relatively well.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import re
 from urllib.parse import urlparse
-import urllib.robotparser
+from urllib.parse import urljoin
 from time import sleep
 import operator
 
@@ -14,16 +14,17 @@ from stopwords import stopwords
 
 import shelve
 
-def discard_url_traps(url):
+def discard_fragment(url):
     new_url = urlparse(url)                
     clean_url = url.replace("#" + new_url.fragment, "")       # Discard URL fragments
-    clean_url = clean_url.replace("?" + new_url.query, "")    # Discard URL query params to avoid traps
     return clean_url
 
 def discard_scheme(url):
     curr_url = urlparse(url)
-    clean_url = url.replace(curr_url.scheme + "://", "")    #Discard Scheme
-    clean_url = url.replace("www.", "")                     #Discard www.
+    clean_url = url.replace(curr_url.scheme + "://" , "")    #Discard Scheme
+    clean_url = clean_url.replace("www.", "")        #Discard www.
+    if clean_url[-1] == "/":
+        clean_url = clean_url[0:-1]
     return clean_url
 
 def strip_stop_words(page_words):
@@ -35,49 +36,43 @@ def strip_stop_words(page_words):
         pageset.add(word)
     return pageset - stopset
 
-def politeness(url):
-    url_parsed = urlparse(url)
-    nice_bot = urllib.robotparser.RobotFileParser(url_parsed.scheme + "://" + url_parsed.netloc + "/robots.txt")
-    nice_bot.read()
-    return nice_bot
-
 def URL_tracking(url):
     # Track unique URLS
     f = open("reports/uniqueurl.txt", "r")
     URL_set = {URL.replace("\n", "") for URL in f}
     f.close()
     init_set_size = len(URL_set)
-    URL_set.add(discard_url_traps(discard_scheme(url)))
+    clean_url = discard_fragment(discard_scheme(url))
+    URL_set.add(clean_url)
     if init_set_size < len(URL_set):
-        f = open("reports/uniqueurl.txt", "w")
-        for page in URL_set:
-            f.write(discard_url_traps(discard_scheme(page)) + "\n")
+        f = open("reports/uniqueurl.txt", "a")
+        f.write(clean_url + "\n")
         f.close()
 
-    # Track subdomains and unique pages within them
-    URL_DB = shelve.open("URLS")
-    url_parsed = urlparse(url)
-    url_netloc = url_parsed.netloc
-    url_netloc = url_netloc.replace("www.","") #discard www
-    ics_domain = ".ics.uci.edu"
-    if ics_domain in url_netloc and url_netloc != ics_domain:
-        subdomain_dict = URL_DB[ics_domain]
-        if url_netloc in subdomain_dict:
-            subdomain_dict[url_netloc] += 1
-        else:
-            subdomain_dict[url_netloc] = 1
-        URL_DB[ics_domain] = subdomain_dict
+        # Track subdomains and unique pages within them
+        URL_DB = shelve.open("URLS")
+        url_parsed = urlparse(url)
+        url_netloc = url_parsed.netloc
+        url_netloc = url_netloc.replace("www.","") #discard www
+        ics_domain = ".ics.uci.edu"
+        if ics_domain in url_netloc and url_netloc != ics_domain:
+            subdomain_dict = URL_DB[ics_domain]
+            if url_netloc in subdomain_dict:
+                subdomain_dict[url_netloc] += 1
+            else:
+                subdomain_dict[url_netloc] = 1
+            URL_DB[ics_domain] = subdomain_dict
 
-    # Write subdomains sorted in alphabetical order to subdomains.txt
-    ics_subdomains = []
-    for main, sub in URL_DB[ics_domain].items():
-        ics_subdomains.append((main,sub))
-    ics_subdomains = sorted(ics_subdomains, key=operator.itemgetter(0))
-    f = open("reports/subdomains.txt", "w")
-    for sub,pages in ics_subdomains:
-        f.write(sub + ", " + str(pages) + "\n")
-    f.close()
-    URL_DB.close()
+        # Write subdomains sorted in alphabetical order to subdomains.txt
+        ics_subdomains = []
+        for main, sub in URL_DB[ics_domain].items():
+            ics_subdomains.append((main,sub))
+        ics_subdomains = sorted(ics_subdomains, key=operator.itemgetter(0))
+        f = open("reports/subdomains.txt", "w")
+        for sub,pages in ics_subdomains:
+            f.write(sub + ", " + str(pages) + "\n")
+        f.close()
+        URL_DB.close()
 
 def word_tracking(url, page_words):
     words_DB = shelve.open("words")
@@ -122,15 +117,11 @@ def scraper(url, resp):
     valid_links = []
     stat_string = str(resp.status)
     if stat_string == "200" or stat_string[0] == "3":
-        if stat_string[0] == "3":
-            with open("reports/redirectedurls.txt","a") as f:
-                f.write(url + "\n")
-                f.close()
         for link in extract_next_links(url, resp):
-            # Check for basic domain rules; allow subdomains and also discard empty links (href = "#")
-            if is_valid(link) and link != "#": 
-                valid_links.append(link)
-            else:
+            if is_valid(link) and link != "#":
+                for rs in resp.raw_response.history:
+                    URL_tracking(rs.url)
+                URL_tracking(url)     # Write link to file 
                 valid_links.append(link)
     return valid_links
 
@@ -147,21 +138,36 @@ def get_text(html_soup):
 def extract_next_links(url, resp):
     links = []
     if resp.raw_response is not None and resp.raw_response.apparent_encoding is not None:
-        URL_tracking(url)     # Write link to file
         html_main = BeautifulSoup(resp.raw_response.content, 'html.parser')
         page_text = get_text(html_main)
         page_blob = None
         if page_text is not None:
             page_blob = TextBlob(page_text)
-        if page_blob is not None and len(page_blob.sentences) > 4: # Only crawl pages with high textual content
+        if page_blob is not None and len(page_blob.sentences) > 2: # Only crawl pages with high textual content
             word_tracking(discard_scheme(url), page_blob.words)
             for anchor in html_main.find_all('a'):      # Retrive all anchor tags in HTML
                 link = anchor.get('href')
-                if is_valid(link):
-                    clean_link = discard_url_traps(link)
+                if link is not None:
+                    clean_link = relative_to_absolute(url, link)
                     if clean_link not in links:             
-                        links.append(clean_link)
+                        links.append(clean_link)    
     return links
+
+def relative_to_absolute(url, link):
+    clean_link = discard_fragment(link)
+    if len(clean_link) >= 1 and re.match(r"^\/(\w+[-.?=&/]?)+", clean_link):
+        clean_link = urljoin(url, clean_link)
+    elif len(clean_link) >= 3 and clean_link.startswith("../"):
+        return_ct = clean_link.count("../")
+        temp_url = url
+        if temp_url[temp_url.rfind("/")] == temp_url[-1]:        # Discard terminal '/' if it exists
+            temp_url = url[0:-1]
+        for i in range(return_ct):                               # Remove return count steps in path from temp_url to adjoin clean_link's relative path
+            temp_url = temp_url[0:temp_url.rfind("/")]
+        clean_link = temp_url + "/" +clean_link.replace("../","")
+    elif len(clean_link) >= 1 and clean_link.startswith("./"):
+        clean_link = url + clean_link
+    return clean_link
 
 def is_valid(url):
     try:
@@ -182,7 +188,7 @@ def is_valid(url):
             or re.match(r"https?://([a-z0-9]+[.])*informatics[.]uci[.]edu(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?", url) \
             or re.match(r"https?://today[.]uci[.]edu/department/information_computer_sciences(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?", url)) \
             and "replytocom" not in url and "pdf" not in url and "event" not in url and "calendar" not in url and "download" not in url \
-            and "photos" not in url 
+            and "photos" not in url and "archive" not in url
     except TypeError:
         print ("TypeError for ", parsed)
         raise
